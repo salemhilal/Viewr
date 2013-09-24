abde7753e9ff3f96de1499d8bbdba97db9518b03
Salem <salemhilal@gmail.com>
2013-09-23 20:19:05 -0400

    Expanded, added index.html. Not quite neat yet.

--- fittr.js
+++ fittr.js
@@ -1,37 +1,47 @@
-
-
-var t = document.getElementsByTagName("img");
-var imgs = [];
-var apiKey = "";
-
-for(var i = 0; i < t.length; i++) {
-  var url = t[i].getAttribute("data-flickr-id");
-  if(src) {
-    imgs.push(new Image(url, t[i]));
-  }
-}
-
-  getJSON(flickrRoot + src, function(data){
-    _.sizes = data.sizes.size;
-  });
-  // Append a flickr id to this to get different sizes, plus url's.
-  var flickrRoot = 
-
 var Fittr = function(key) {
 
   var _ = this;
   var flickrRoot = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=" + key + "&format=json&nojsoncallback=1&photo_id=";
-  
+
+  var t = document.getElementsByTagName("img");
+  var imgs = [];
+  var apiKey = "";
+
+  for(var i = 0; i < t.length; i++) {
+    var id = t[i].getAttribute("data-flickr-id");
+    if(id) {
+      _.getJsonById(id, function(data, err) {
+        if(err) {
+          console.error("Failed to retrieve image: " + id, err);
+        }
+        // Create a new image, initialize it, add it to the list of images.
+        var currImg = new Image(id, t[i], apiKey, data);
+        currImg.init;
+        imgs.push(currImg);
+      });
+      
+    }
+  }
+
   // Don't need no stinkin' jQuery here.
-  _.getJson = function getJSON(url, fn) {
+  _.getJson = function (url, fn) {
     
     var r = new XMLHttpRequest();
     r.open("GET", url, true);
 
     r.onreadystatechange = function() {
       // readyState === 4 => ruest finished, response is ready
-      if(r.readyState === 4 && r.status === 200) {
-        fn(JSON.parse(r.responseText));
+      if (r.readyState === 4) {
+        if (r.status === 200){
+          fn(JSON.parse(r.responseText));
+        }
+        else{
+          console.error(r);
+          fn(null, {
+            status: r.status,
+            statusText: r.statusText
+          }); 
+        }
       }
     }
     r.send();
@@ -43,13 +53,14 @@ var Fittr = function(key) {
 
 }
 
-var Image = function(src, elem, apiKey){
+var Image = function(id, elem, apiKey, data){
   var _ = this;
 
 
 
   _.elem = elem; // Dom element of image.
-  _.flickrId = src;   // url of image.
+  _.flickrId = id;   // url of image.
+  _.sizes = data.sizes.size;
   
   // Get hardcoded styles
   _.cssWidth = _.elem.style.width;
@@ -58,11 +69,11 @@ var Image = function(src, elem, apiKey){
 
   _.sizes; //Payload data
   _.ratio; // width / height
+  _.init = function() {
 
-
-
-
+  }
 }
 
 
-9467631964
+// 9467631964
+
