9cd14e624073b7cd765dd03f290f65e7140767e2
Salem <salemhilal@gmail.com>
2013-09-26 03:50:25 -0400

    That's quite enough for one day. Go to bed, Salem.

--- README.md
+++ README.md
@@ -7,7 +7,16 @@ Simply annotate your images with the id's of flickr images.
  * Looks great on retina devices!
  * Doesn't load unnecessarily large images! 
  * Entirely client-side!
+ * No dependencies!
  * Recommended by 9/10 dentists!
+
+
+Background
+----------
+
+Loading responsive images is a bit difficult, considering you need to have access to multiple
+sizes of the same image. If you load a retina-quality image for every user, the ones without 
+retina screens suffer from awful load times. 
 
 Usage
 -----
@@ -28,7 +37,8 @@ Start Fittr up inside ```<script>``` tags right after that, as follows:
 
 
 Loading an image from flickr is easy. In your image tag, don't add a ```src``` attribute. 
-Instead, add a  ```data-flickr-id``` attribute, and set it equal to the id of an image on Flickr:
+Instead, add a  ```data-flickr-id``` attribute, and set it equal to the id of an image on Flickr. 
+Be sure to set a width, either with CSS or inline:
 
     <img data-flickr-id="9467631964" width="500" alt="Flickr photo"></img>
 
@@ -40,12 +50,26 @@ Options
 
 You can pass a number of options to Fittr:
 
+#### filter ####
+Only runs on img tags with a given selector (id or class). This is good if you want to use different
+options on different groups of images.
+
+    default: null
+    options: any selector (string)
+
 #### increment ####
 Determines whether to load a half resolution image first, and load a full resolution one later.
 Good if you're loading large images and want to display content as soon as possible.
     
-    default: false
+    default: true
     options: boolean (true / false)
+
+#### incRatio ####
+The ratio between the size of the final image loaded the incremental image load. 
+Defaults to three, which means that the first image to load is 1/3 the resolution of the final image. 
+
+    default: 3
+    options: any number
 
 #### square ####
 Look only for square images (note that flickr only offers square images of size 75x75 and 150x150).
@@ -54,13 +78,23 @@ Look only for square images (note that flickr only offers square images of size 
     options: boolean (true / false)
 
 
-Extras
-------
+Notes
+-----
 
-I drew a lot of inspiration from [https://responsive.io/][1] and have had nothing but good experiences with 
+Fittr makes sure the visability of every image is set to ```visable``` as soon as it has a valid ```src```
+attribute. If you want to prevent users from seeing unstyled images, set them to be hidden before they're 
+properly loaded by adding the following CSS:
+
+    img[data-flickr-id] {
+        visibility: hidden;
+    }
+
+I drew a lot of inspiration from [responsive.io/][1] and have had nothing but good experiences from 
 their service. 
 
-If you notice any bugs, or if you have any feedback or feature requests, please feel free to [contact me]!
+If you notice any bugs, or if you have any feedback or feature requests, please feel free to [contact me][2]!
+
+Thanks a ton to [Sandra Chen][3] for letting me use her awesome photos to showcase Fittr. 
 
 Finally, note that while this script will load any publically available image from flickr, this does not 
 mean that you sould use other people's photos without permission (or unless they say it's ok).
@@ -70,3 +104,4 @@ mean that you sould use other people's photos without permission (or unless they
 
 [1]: https://responsive.io/
 [2]: http://technoheads.org/about/
+[3]: http://www.sandra-chen.com/
--- fittr.js
+++ fittr.js
@@ -1,9 +1,11 @@
 var Fittr = function(key, opts) {
+  var _ = this;
   var opts = opts || {};
   var flickrRoot = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=" + key + "&format=json&nojsoncallback=1&photo_id=";
 
   var images = [];
   var apiKey = "";
+  var filter = opts.filter || null;
 
   // Requests JSON data from given url.
   var getJson = function (url, fn) {
@@ -34,26 +36,37 @@ var Fittr = function(key, opts) {
     getJson(flickrRoot + id, fn);
   }
 
-  var rawImgs = document.getElementsByTagName("img");
-  for(var i = 0; i < rawImgs.length; i++) {
-    var id = rawImgs[i].getAttribute("data-flickr-id");
-    if(id) {
-      var elem = rawImgs[i];
-      getJsonById(id, function(data, err) {
-        if(err) {
-          console.error("Failed to retrieve image: " + id, err);
-        }
-        // Create a new image, initialize it, add it to the list of images.
-        var currImg = new FittrImage(elem, data, {
-          increment: opts.increment || false,
-          square: opts.square || false
+  _.reload = function(){
+    // Get the list of image tags, filtering if need be.
+    var rawImgs;
+    if (!filter) {
+      rawImgs = document.getElementsByTagName("img");
+    }
+    else {
+      rawImgs = document.querySelectorAll(filter);
+    }
+
+    for(var i = 0; i < rawImgs.length; i++) {
+      var id = rawImgs[i].getAttribute("data-flickr-id");
+      var tag = rawImgs[i].tagName.toLowerCase();
+      if(id &&  tag == "img") {
+        var elem = rawImgs[i];
+        getJsonById(id, function(data, err) {
+          if(err) {
+            console.error("Failed to retrieve image: " + id, err);
+          }
+          // Create a new image, initialize it, add it to the list of images.
+          var currImg = new FittrImage(elem, data, opts);
+          currImg.init();
+          images.push(currImg);
         });
-        currImg.init();
-        images.push(currImg);
-      });
-      
+        
+      }
     }
   }
+
+  _.reload();
+
 }
 
 var FittrImage = function(elem, data, opts){
@@ -61,13 +74,14 @@ var FittrImage = function(elem, data, opts){
 
   var _ = this;
 
-  var elem = elem;                            // Dom element of image.
-  _.sizes = data.sizes.size;                  // Data from Flickr
-  var square = opts.square || false;          // Do we want a square image?
-  var density = window.devicePixelRatio || 1; // Pixel density. Default to 1.
-  var pick;                                   // Holds the image we picked. 
-  var increment = opts.increment || false;    // Whether or not to load progressively larger images.
-  var pickIncrement;                          // If increment is on, the image to increment from.
+  var elem = elem;                             // Dom element of image.
+  var sizes = data.sizes.size;                 // Data from Flickr
+  var square = opts.square || false;           // Do we want a square image?
+  var density = window.devicePixelRatio || 1;  // Pixel density. Default to 1.
+  var pick;                                    // Holds the image we picked. 
+  var increment = opts.increment || true;      // Whether or not to load progressively larger images.
+  var pickIncrement;                           // If increment is on, the image to increment from.
+  var incRatio = opts.incRatio || 2;
 
   // Calculate necessary width. We don't use height, as we don't do any
   // pre-processing on the image and so wouldn't need it.
@@ -78,33 +92,30 @@ var FittrImage = function(elem, data, opts){
   _.init = function() {
 
     // Find the biggest image that will fit correctly.
-    for(var i = 0; i < _.sizes.length; i++) {
+    for(var i = 0; i < sizes.length; i++) {
       
       // If we're not looking for square images, skip over them.
-      if(_.sizes[i].label.toLowerCase().indexOf("square") !== -1 && !square)
+      if(sizes[i].label.toLowerCase().indexOf("square") !== -1 && !square)
         continue;
       // Likewise, if we are, looking for square images, skip over everything else.
-      if(_.sizes[i].label.toLowerCase().indexOf("square") == -1 && square)
+      if(sizes[i].label.toLowerCase().indexOf("square") == -1 && square)
         continue;
 
       // Update the one we pick
-      pick = _.sizes[i];
+      pick = sizes[i];
       if(pick.width >= _.width)
         break;
 
-      if(increment                          // We want to increment
-        && !pickIncrement                   // We haven't found an inc image yet
-        && pick.width >= _.width / 2) {     // This one will do.
-        console.log(pick.source);
+      if(increment                              // We want to increment
+        && !pickIncrement                       // We haven't found an inc image yet
+        && pick.width >= _.width / incRatio) {  // This one will do.
         pickIncrement = pick;
         elem.setAttribute("src", pick.source);
       }
 
     }
 
-    if(increment) {
-      console.log(pickIncrement.width);
-      console.log(pick.width);
+    if(increment && pickIncrement) {
       elem.onload = function(){
         elem.setAttribute("src", pick.source);
       }
